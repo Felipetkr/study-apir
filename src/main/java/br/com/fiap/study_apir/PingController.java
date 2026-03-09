96268cfd73c5a7e2687bd6b5d9b406c68b07e260
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 20:27:07 -0300

    feat: add new controller

--- src/main/java/br/com/fiap/study_apir/PingController.java
+++ src/main/java/br/com/fiap/study_apir/PingController.java
@@ -3,11 +3,27 @@ package br.com.fiap.study_apir;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//sempre instalar Extension Pack for Java
 @RestController
-@RequestMapping
+@RequestMapping("/ping")
 public class PingController {
-    @GetMapping("/ping")
+    @GetMapping("/rota")
     public String ping(){
         return "pong";
+    }
+
+    @GetMapping("/rota1")
+    public String rota1(){
+        return "rt";
+    }
+
+    @GetMapping("/rota2")
+    public String rota2(){
+        return "oa";
+    }
+
+    @GetMapping()
+    public String rota3(){
+        return "o";
     }
 }
